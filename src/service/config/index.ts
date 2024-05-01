47dc00759f30987d2a55c8ae7139ca485509e601
coderLH <codelh@e.pdsu.edu.cn>
2024-05-01 22:51:14 +0800

    fix(dep-mo): 修改了一个符号

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -2,12 +2,11 @@ let BASE_URL = ''
 //MODE  PROD  DEV SSR
 if (import.meta.env.PROD) {
   // 生产环境
-  // BASE_URL = 'http://codercba.com:5000'
-  BASE_URL = 'http://localhost:8888'
+  BASE_URL = 'http://120.26.240.157:8888'
 } else {
   // 开发环境
-  // BASE_URL = 'http://codercba.com:5000'
-  BASE_URL = 'http://localhost:8888'
+  // BASE_URL = 'http://localhost:8888'
+  BASE_URL = 'http://120.26.240.157:8888'
 }
 
 export const TIME_OUT = 10000
